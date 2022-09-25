e9962e5a042eaacc77c5b11fa4f1cc992b8c7c12
Adekoya Samuel <99253410+Samdek@users.noreply.github.com>
2022-09-25 21:19:59 +0100

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,23 +1,27 @@
+import { Link } from "react-router-dom"
+
 export default function Login() {
     return(
-        <div>
-            <form className="login_form">
-                <h2>Sign in with your email</h2>
-                <p>Dont have an account? <a href="">Sign in</a></p>
+        <main className="App">
+            <div>
+                <form className="login_form">
+                    <h2>Sign in with your email</h2>
+                    <p>Dont have an account? <Link to="/register">Sign up</Link></p>
 
-                <div className="input_container">
-                <label htmlFor="email">Email address</label>
-                    <input id="email" type="email" />
-                </div>
-                <div className="input_container">
-                    <label htmlFor="password">Password</label>
-                    <input id="password" type="password" />
-                </div>
+                    <div className="input_container">
+                    <label htmlFor="email">Email address</label>
+                        <input id="email" type="email" />
+                    </div>
+                    <div className="input_container">
+                        <label htmlFor="password">Password</label>
+                        <input id="password" type="password" />
+                    </div>
 
-                <button className="login-btn">Continue</button>
-            </form>
-            <a href="" className="forgot_password_link">Forgot your password?</a>
-        </div>  
+                    <button className="login-btn">Continue</button>
+                </form>
+                <a href="" className="forgot_password_link">Forgot your password?</a>
+            </div>
+        </main>
     )
 
 }
