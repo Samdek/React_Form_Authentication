67d02806b5d78b6dd6ca1e79927bfac1e197db31
Adekoya Samuel <99253410+Samdek@users.noreply.github.com>
2022-09-25 20:35:14 +0100

    added error page

---
+++ src/Pageerror.js
@@ -0,0 +1,9 @@
+export default function Pageerror() {
+    return(
+        <div className="error-page">
+            <h1>404</h1>
+            <h3>Page Not Found</h3>
+            <p>Check if there is typo in your spelling</p>
+        </div>
+    )
+}
